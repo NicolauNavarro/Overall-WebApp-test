8cf4eb7f30b7f329dbd566ff374436945dabb146
Nicolau <149939145+NicolauNavarro@users.noreply.github.com>
2024-08-13 10:53:33 +0000

    Feal frameworks

--- client/app/page.tsx
+++ client/app/page.tsx
@@ -1,13 +1,13 @@
 import Image from "next/image";
 import styles from "./page.module.css";
 
-export default function home() {
+export default function Home() {
   return (
     <main className={styles.main}>
       <div className={styles.description}>
         <p>
           Get started by editing&nbsp;
-          <code className={styles.code}>app/page.js</code>
+          <code className={styles.code}>app/page.tsx</code>
         </p>
         <div>
           <a
